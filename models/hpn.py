3e6479ff6dc325bdcf02958addd84640d78ddd3f
Chin-Yun Yu <lolimaster.cs03@nctu.edu.tw>
2023-03-26 17:03:19 +0800

    fix: apply voicing after osc

--- models/hpn.py
+++ models/hpn.py
@@ -30,11 +30,11 @@ class HarmonicPlusNoiseSynth(nn.Module):
 
     def forward(
         self,
+        ctx: TimeContext,
         phase_params: Tuple[Tensor, Optional[Tensor]],
         harm_osc_params: Tuple[Tensor, ...],
         harm_filt_params: Tuple[Tensor, ...],
         noise_filt_params: Tuple[Tensor, ...],
-        ctx: TimeContext,
         noise_params: Tuple[Tensor, ...] = (),
     ) -> Tensor:
         """
@@ -44,14 +44,15 @@ class HarmonicPlusNoiseSynth(nn.Module):
         phase, *_ = phase_params
         assert torch.all(phase >= 0) and torch.all(phase <= 0.5)
         upsampled_phase = linear_upsample(phase, ctx)
+        # Time-varying components
+        harm_osc = self.harm_oscillator(upsampled_phase, *harm_osc_params, ctx=ctx)
+
         if len(_):
             voicing = _[0]
             assert torch.all(voicing >= 0) and torch.all(voicing <= 1)
             upsampled_voicing = linear_upsample(voicing, ctx)
-            upsampled_phase = upsampled_phase * upsampled_voicing
+            harm_osc = harm_osc * upsampled_voicing[:, : harm_osc.shape[1]]
 
-        # Time-varying components
-        harm_osc = self.harm_oscillator(upsampled_phase, *harm_osc_params, ctx=ctx)
         noise = self.noise_generator(harm_osc, *noise_params, ctx=ctx)
         if self.harm_filter is not None:
             harm_osc = self.harm_filter(harm_osc, *harm_filt_params, ctx=ctx)

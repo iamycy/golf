7d86694ff808ee2c4dd5ce7da61fa28ba422e891
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-05-30 21:34:22 +0100

    draft: lpcnet

---
+++ lpcnet/__init__.py
@@ -0,0 +1,176 @@
+from typing import Optional
+import torch
+from torch import Tensor
+import torch.nn as nn
+from torchaudio.transforms import MuLawEncoding, MuLawDecoding
+from typing import Optional, Tuple
+
+
+class ContinuousMuLawEncoding(MuLawEncoding):
+    def forward(self, x: Tensor) -> Tensor:
+        mu = self.quantization_channels - 1.0
+        mu = torch.tensor(mu, dtype=x.dtype)
+        x_mu = torch.sign(x) * torch.log1p(mu * torch.abs(x)) / torch.log1p(mu)
+        x_mu = (x_mu + 1) / 2 * mu
+        return x_mu
+
+
+class ContinuousMuLawDecoding(MuLawDecoding):
+    def forward(self, x_mu: Tensor) -> Tensor:
+        mu = self.quantization_channels - 1.0
+        mu = torch.tensor(mu, dtype=x_mu.dtype)
+        x = (x_mu / mu) * 2 - 1
+        x = torch.sign(x) * (torch.exp(torch.abs(x) * torch.log1p(mu)) - 1) / mu
+        return x
+
+
+class InterpolatedEmbedding(nn.Embedding):
+    def forward(self, x: Tensor) -> Tensor:
+        num_embeddings = self.num_embeddings
+        assert x.dtype == torch.float32
+        assert torch.all(x >= 0), x.min().item()
+        assert torch.all(x <= (self.num_embeddings - 1)), x.max().item()
+
+        raw_idx = x * (num_embeddings - 1)
+        lower_idx = torch.floor(raw_idx).long().clip(0, num_embeddings - 2)
+        upper_idx = lower_idx + 1
+        p = raw_idx - lower_idx
+        p = p.unsqueeze(-1)
+        lower_embedding = super().forward(lower_idx)
+        upper_embedding = super().forward(upper_idx)
+        return lower_embedding * (1 - p) + upper_embedding * p
+
+
+class SampleNet(nn.Module):
+    def __init__(
+        self,
+        quantization_channels: int = 256,
+        condition_channels: int = 128,
+        a_channels: int = 192,
+        b_channels: int = 32,
+    ):
+        super().__init__()
+
+        self.a_channels = a_channels
+        self.b_channels = b_channels
+
+        self.embedding_s = InterpolatedEmbedding(
+            quantization_channels, quantization_channels
+        )
+        self.embedding_e = InterpolatedEmbedding(
+            quantization_channels, quantization_channels
+        )
+        self.embedding_p = InterpolatedEmbedding(
+            quantization_channels, quantization_channels
+        )
+        # for i in "urh":
+        #     for j in "spe":
+        #         self.register_module(
+        #             f"embedding_{i}_{j}",
+        #             InterpolatedEmbedding(quantization_channels, a_channels),
+        #         )
+
+        # self.W_a = nn.Linear(a_channels, a_channels * 3)
+        # self.cond_linear = nn.Linear(condition_channels + 3 * quantization_channels, a_channels * 3, bias=False)
+        self.gru_a = nn.GRU(
+            condition_channels + 3 * quantization_channels,
+            a_channels,
+            bias=False,
+            batch_first=True,
+        )
+        self.gru_b = nn.GRU(a_channels, b_channels, bias=False, batch_first=True)
+
+        self.a = nn.Parameter(torch.randn(quantization_channels * 2))
+        self.fc = nn.Sequential(
+            nn.Linear(b_channels, quantization_channels * 2),
+            nn.Tanh(),
+        )
+
+    def forward(self, f: Tensor, p: Tensor, s_prev: Tensor, e_prev: Tensor) -> Tensor:
+        # GRU A
+        # g_u, g_r, g_h = torch.chunk(self.cond_linear(f), 3, dim=-1)
+
+        # u_s = self.embedding_u_s(s_prev)
+        # u_p = self.embedding_u_p(p)
+        # u_e = self.embedding_u_e(e_prev)
+        # r_s = self.embedding_r_s(s_prev)
+        # r_p = self.embedding_r_p(p)
+        # r_e = self.embedding_r_e(e_prev)
+        # h_s = self.embedding_h_s(s_prev)
+        # h_p = self.embedding_h_p(p)
+        # h_e = self.embedding_h_e(e_prev)
+
+        # batch, T, _ = f.shape
+        # h = f.new_zeros(batch, self.a_channels)
+        # outputs = []
+        # for i in range(T):
+        #     h_u, h_r, h_h = torch.chunk(self.W_a(h), 3, dim=-1)
+        #     u = torch.sigmoid(g_u[:, i] + h_u + u_s[:, i] + u_p[:, i] + u_e[:, i])
+        #     r = torch.sigmoid(g_r[:, i] + h_r + r_s[:, i] + r_p[:, i] + r_e[:, i])
+        #     h_hat = torch.tanh(g_h[:, i] + h_h * r + h_s[:, i] + h_p[:, i] + h_e[:, i])
+        #     h = (1 - u) * h + u * h_hat
+        #     outputs.append(h)
+
+        # # GRU B
+        # x = torch.stack(outputs, dim=1)
+        # h = x.new_zeros(batch, self.b_channels)
+        # outputs = []
+        # for i in range(T):
+        #     h = self.gru_b(x[:, i], h)
+        #     outputs.append(h)
+
+        # h = torch.stack(outputs, dim=1)
+
+        p = self.embedding_p(p)
+        s = self.embedding_s(s_prev)
+        e = self.embedding_e(e_prev)
+        h = torch.cat([f, p, s, e], dim=-1)
+        h, _ = self.gru_a(h)
+        h, _ = self.gru_b(h)
+
+        h = self.fc(h) * self.a
+        h = h.view(h.shape[0], h.shape[1], -1, 2).sum(dim=-1)
+
+        return h
+
+    def sample_forward(
+        self,
+        f: Tensor,
+        p: Tensor,
+        s_prev: Tensor,
+        e_prev: Tensor,
+        states: Optional[Tuple[Tensor, ...]] = None,
+    ):
+
+        batch, _ = f.shape
+
+        if states is None:
+            state_a = f.new_zeros(batch, self.a_channels)
+            state_b = f.new_zeros(batch, self.b_channels)
+        else:
+            state_a, state_b = states
+
+        g_u, g_r, g_h = torch.chunk(self.cond_linear(f), 3, dim=-1)
+
+        u_s = self.embedding_u_s(s_prev)
+        u_p = self.embedding_u_p(p)
+        u_e = self.embedding_u_e(e_prev)
+        r_s = self.embedding_r_s(s_prev)
+        r_p = self.embedding_r_p(p)
+        r_e = self.embedding_r_e(e_prev)
+        h_s = self.embedding_h_s(s_prev)
+        h_p = self.embedding_h_p(p)
+        h_e = self.embedding_h_e(e_prev)
+
+        h_u, h_r, h_h = torch.chunk(self.W_a(state_a), 3, dim=-1)
+        u = torch.sigmoid(g_u + h_u + u_s + u_p + u_e)
+        r = torch.sigmoid(g_r + h_r + r_s + r_p + r_e)
+        h_hat = torch.tanh(g_h + h_h * r + h_s + h_p + h_e)
+        state_a = (1 - u) * state_a + u * h_hat
+
+        state_b = self.gru_b(state_a, state_b)
+
+        h = self.fc(state_b) * self.a
+        h = h.view(batch, -1, 2).sum(dim=-1)
+
+        return h, (state_a, state_b)
---
+++ ltng/lpcnet.py
@@ -0,0 +1,196 @@
+import lightning.pytorch as pl
+from lightning.pytorch.cli import LightningCLI, LightningArgumentParser
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+from typing import List, Dict, Tuple, Callable, Union
+from torchaudio.transforms import MelSpectrogram
+import numpy as np
+import yaml
+import math
+from importlib import import_module
+from frechet_audio_distance import FrechetAudioDistance
+from torchaudio.transforms import Preemphasis, Deemphasis
+
+from models.utils import get_window_fn, rc2lpc, fir_filt, AudioTensor
+from lpcnet import SampleNet, ContinuousMuLawDecoding, ContinuousMuLawEncoding
+
+
+class LPCNetVocoderCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
+        parser.link_arguments(
+            "model.hop_length", "model.feature_trsfm.init_args.hop_length"
+        )
+
+        parser.link_arguments(
+            "model.sample_rate", "model.feature_trsfm.init_args.sample_rate"
+        )
+        parser.link_arguments(
+            "model.window",
+            "model.feature_trsfm.init_args.window",
+        )
+
+        parser.link_arguments(
+            "model.quantization_channels",
+            "model.sample_decoder.init_args.quantization_channels",
+        )
+
+
+class LPCNetVocoder(pl.LightningModule):
+    def __init__(
+        self,
+        feature_trsfm: torch.nn.Module,
+        frame_decoder: torch.nn.Module,
+        sample_decoder: SampleNet,
+        lpc_order: int = 22,
+        quantization_channels: int = 256,
+        alpha: float = 0.85,
+        window: str = "hanning",
+        sample_rate: int = 24000,
+        hop_length: int = 120,
+    ):
+        super().__init__()
+
+        # self.save_hyperparameters()
+
+        self.frame_decoder = frame_decoder
+        self.sample_decoder = sample_decoder
+        self.feature_trsfm = feature_trsfm
+        self.mu_enc = ContinuousMuLawEncoding(quantization_channels)
+        self.mu_dec = ContinuousMuLawDecoding(quantization_channels)
+        self.pre_emphasis = Preemphasis(alpha)
+        self.de_emphasis = Deemphasis(alpha)
+
+        self.sample_rate = sample_rate
+        self.hop_length = hop_length
+        self.lpc_order = lpc_order
+
+        mu = quantization_channels - 1.0
+        self.regularizer = (
+            lambda x: (x - 0.5 * mu).abs().mean() * math.log1p(mu) / mu * 2
+        )
+
+    def forward(self, feats: torch.Tensor):
+        (f0, *other_params, voicing_logits) = self.encoder(feats)
+
+        phase = f0 / self.sample_rate
+        if voicing_logits is not None:
+            voicing = torch.sigmoid(voicing_logits)
+        else:
+            voicing = None
+
+        return (
+            f0,
+            self.decoder(phase, *other_params, voicing=voicing),
+            voicing,
+        )
+
+    def on_train_start(self) -> None:
+        if self.logger is not None:
+            self.logger.watch(
+                self.frame_decoder, log_freq=1000, log="all", log_graph=False
+            )
+            self.logger.watch(
+                self.sample_decoder, log_freq=1000, log="all", log_graph=False
+            )
+
+    def on_train_end(self) -> None:
+        if self.logger is not None:
+            self.logger.experiment.unwatch(self.frame_decoder)
+            self.logger.experiment.unwatch(self.sample_decoder)
+
+    def interp_loss(self, e_mu, logits):
+        q = logits.shape[1]
+        e_mu = e_mu.unsqueeze(1)
+        lower_idx = torch.floor(e_mu).long().clip(0, q - 2)
+        upper_idx = lower_idx + 1
+        p = e_mu - lower_idx
+        log_prob = F.log_softmax(logits, dim=1)
+        selected_log_probs = (
+            torch.gather(log_prob, 1, lower_idx) * (1 - p)
+            + torch.gather(log_prob, 1, upper_idx) * p
+        )
+        return -selected_log_probs.mean() + self.regularizer(e_mu)
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+
+        s = self.pre_emphasis(x)
+        # s = torch.clip(s, -1, 1)
+        assert ~torch.any(torch.isnan(s)), "NaN in input signal"
+        feats = self.feature_trsfm(x)
+
+        f = self.frame_decoder(feats)
+        lpc = rc2lpc(f[..., : self.lpc_order].tanh())
+
+        f = AudioTensor(f, hop_length=self.hop_length).reduce_hop_length().as_tensor()
+        upsampled_lpc = (
+            AudioTensor(lpc, hop_length=self.hop_length).reduce_hop_length().as_tensor()
+        )
+        minimum_length = min(upsampled_lpc.shape[1], s.shape[1])
+        s = s[:, :minimum_length]
+        upsampled_lpc = upsampled_lpc[:, :minimum_length]
+        f = f[:, :minimum_length]
+        weight = torch.cat([torch.ones_like(upsampled_lpc[..., :1]), upsampled_lpc], 2)
+        e = fir_filt(s, weight)
+        assert ~torch.any(torch.isnan(e)), "NaN in excitation signal"
+        p = s - e
+
+        p_mu = self.mu_enc(p.clip(-1, 1))
+        e_mu = self.mu_enc(e.clip(-1, 1))
+        s_mu = self.mu_enc(s.clip(-1, 1))
+
+        pred_e_logits = self.sample_decoder(
+            f[:, 1:], p_mu[:, 1:], s_mu[:, :-1], e_mu[:, :-1]
+        )
+
+        loss = self.interp_loss(e_mu[:, 1:], pred_e_logits.transpose(1, 2))
+
+        self.log("train_loss", loss, prog_bar=True, sync_dist=True)
+        return loss
+
+    def on_validation_epoch_start(self) -> None:
+        self.tmp_val_outputs = []
+
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+
+        s = self.pre_emphasis(x)
+        feats = self.feature_trsfm(x)
+
+        f = self.frame_decoder(feats)
+        lpc = rc2lpc(f[..., : self.lpc_order].tanh())
+
+        f = AudioTensor(f, hop_length=self.hop_length).reduce_hop_length().as_tensor()
+        upsampled_lpc = (
+            AudioTensor(lpc, hop_length=self.hop_length).reduce_hop_length().as_tensor()
+        )
+        minimum_length = min(upsampled_lpc.shape[1], s.shape[1])
+        s = s[:, :minimum_length]
+        upsampled_lpc = upsampled_lpc[:, :minimum_length]
+        f = f[:, :minimum_length]
+        weight = torch.cat([torch.ones_like(upsampled_lpc[..., :1]), upsampled_lpc], 2)
+        e = fir_filt(s, weight)
+        p = s - e
+
+        p_mu = self.mu_enc(p)
+        e_mu = self.mu_enc(e)
+        s_mu = self.mu_enc(s)
+
+        pred_e_logits = self.sample_decoder(
+            f[:, 1:], p_mu[:, 1:], s_mu[:, :-1], e_mu[:, :-1]
+        )
+
+        loss = self.interp_loss(e_mu[:, 1:], pred_e_logits.transpose(1, 2))
+
+        self.tmp_val_outputs.append((loss,))
+
+        return loss
+
+    def on_validation_epoch_end(self) -> None:
+        outputs = self.tmp_val_outputs
+        avg_loss = sum(x[0] for x in outputs) / len(outputs)
+
+        self.log("val_loss", avg_loss, prog_bar=True, sync_dist=True)
+
+        delattr(self, "tmp_val_outputs")

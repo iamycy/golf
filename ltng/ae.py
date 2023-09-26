d8e597456aa11490779d56b74cfcace6616eef95
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-09-26 10:29:48 +0100

    refactor: ae trainer

--- ltng/ae.py
+++ ltng/ae.py
@@ -3,7 +3,7 @@ from lightning.pytorch.cli import LightningCLI, LightningArgumentParser
 import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
-from typing import List, Dict, Tuple, Callable, Union
+from typing import List, Dict, Tuple, Callable, Union, Any
 from torchaudio.transforms import MelSpectrogram
 import numpy as np
 import yaml
@@ -19,228 +19,126 @@ from models.synth import WrappedPhaseDownsampledIndexedGlottalFlowTable
 from models.noise import NoiseInterface
 from models.filters import SampleBasedLTVMinimumPhaseFilter, LTVZeroPhaseFIRFilter
 from models.crepe import CREPE
+from models.sf import SourceFilterSynth
+from models.hpn import HarmonicPlusNoiseSynth
 from .vocoder import ScaledLogMelSpectrogram
 
 
 class VoiceAutoEncoderCLI(LightningCLI):
     def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
-        parser.link_arguments(
-            "model.hop_length", "model.feature_trsfm.init_args.hop_length"
-        )
-
-        parser.link_arguments(
-            "model.sample_rate", "model.feature_trsfm.init_args.sample_rate"
-        )
-        parser.link_arguments(
-            "model.window",
-            "model.feature_trsfm.init_args.window",
-        )
+        return
 
 
 class VoiceAutoEncoder(pl.LightningModule):
     def __init__(
         self,
-        feature_trsfm: ScaledLogMelSpectrogram,
-        feature_encoder_type: str,
-        feature_encoder_kwargs: Dict,
-        # f0_encoder: nn.Module,
-        harm_oscillator: WrappedPhaseDownsampledIndexedGlottalFlowTable,
-        noise_generator: NoiseInterface,
-        noise_filter: LTVZeroPhaseFIRFilter,
-        end_filter: SampleBasedLTVMinimumPhaseFilter,
+        decoder: Union[SourceFilterSynth, HarmonicPlusNoiseSynth],
         criterion: nn.Module,
-        window: str = "hanning",
+        encoder_class_path: str,
+        encoder_init_args: Dict = {},
         sample_rate: int = 24000,
-        hop_length: int = 120,
-        f0_min: float = 60,
-        f0_max: float = 1000,
-        l1_loss_weight: float = 0.0,
+        detach_f0: bool = False,
+        detach_voicing: bool = False,
+        train_with_true_f0: bool = True,
         f0_loss_weight: float = 1.0,
+        voicing_loss_weight: float = 1.0,
     ):
         super().__init__()
 
+        self.decoder = decoder
         self.criterion = criterion
-        self.feature_trsfm = feature_trsfm
-        self.f0_encoder = CREPE(1, 513)
-        self.offset_downsampler = nn.Sequential(
-            nn.ReLU(),
-            nn.AvgPool1d(
-                kernel_size=17,
-                stride=8,
-                padding=8,
-            ),
-            nn.Conv1d(
-                in_channels=512,
-                out_channels=256,
-                kernel_size=1,
-            ),
-            nn.ReLU(),
-            nn.Conv1d(
-                in_channels=256,
-                out_channels=1,
-                kernel_size=1,
-            ),
-        )
 
-        self.harm_oscillator = harm_oscillator
-        self.noise_generator = noise_generator
-        self.noise_filter = noise_filter
-        self.end_filter = end_filter
-
-        ctrl_fns = [
-            self.harm_oscillator.ctrl,
-            self.noise_generator.ctrl,
-            self.noise_filter.ctrl,
-            self.end_filter.ctrl,
-        ]
-        split_trsfm = DUMMY_SPLIT_TRSFM
-        for ctrl_fn in ctrl_fns[::-1]:
-            split_trsfm = ctrl_fn(split_trsfm)
-        self.split_tuples, self.trsfms = split_trsfm((), ())
-        self._split_size = sum(self.split_tuples, ())
-
-        module_path, class_name = feature_encoder_type.rsplit(".", 1)
+        module_path, class_name = encoder_class_path.rsplit(".", 1)
         module = import_module(module_path)
-
-        self.feature_encoder = getattr(module, class_name)(
-            out_channels=sum(self._split_size), **feature_encoder_kwargs
+        split_sizes, trsfms, args_keys = self.decoder.get_split_sizes_and_trsfms()
+        self.encoder = getattr(module, class_name)(
+            split_sizes=split_sizes,
+            trsfms=trsfms,
+            args_keys=args_keys,
+            **encoder_init_args
         )
 
         self.sample_rate = sample_rate
-        self.hop_length = hop_length
-        self.l1_loss_weight = l1_loss_weight
         self.f0_loss_weight = f0_loss_weight
+        self.voicing_loss_weight = voicing_loss_weight
+        self.detach_f0 = detach_f0
+        self.detach_voicing = detach_voicing
+        self.train_with_true_f0 = train_with_true_f0
 
-        self.log_f0_min = math.log(f0_min)
-        self.log_f0_max = math.log(f0_max)
+    def forward(
+        self,
+        x: AudioTensor = None,
+        f0: AudioTensor = None,
+        params: Dict[str, Union[AudioTensor, Tuple[AudioTensor]]] = {},
+    ) -> Tuple[AudioTensor, Dict[str, Union[AudioTensor, Tuple[AudioTensor]]]]:
+        if x is not None:
+            enc_params: Dict = self.encoder(x, f0=f0)
+            params.update(enc_params)
 
-    def logits2f0(self, logits: AudioTensor) -> AudioTensor:
-        return torch.exp(
-            torch.sigmoid(logits) * (self.log_f0_max - self.log_f0_min)
-            + self.log_f0_min
-        )
+            if "phase" not in params:
+                params["phase"] = params["f0"] / self.sample_rate
 
-    def forward(self, x: torch.Tensor):
-        # wrapped_phase = self.f0_encoder(x.unsqueeze(1)).sigmoid().squeeze(1)
+            params.pop("f0", None)
 
-        # # get smooth wrapped_phase
-        # pitch_mark = torch.diff(wrapped_phase, 1, 1) < -0.0
-        # consecutive_pitch_mark = (pitch_mark & torch.roll(pitch_mark, 1, 1)) | (
-        #     pitch_mark & torch.roll(pitch_mark, 2, 1)
-        # )
-        # pitch_mark[consecutive_pitch_mark] = False
+            voicing_logits = params.pop("voicing_logits", None)
+            if voicing_logits is not None:
+                params["voicing"] = torch.sigmoid(voicing_logits)
 
-        # batch_idx, time_idx = torch.nonzero(pitch_mark, as_tuple=True)
-        # periods = torch.diff(time_idx)
-        # valid_mask = torch.diff(batch_idx) == 0
-
-        # mark_values = wrapped_phase[batch_idx, time_idx]
-        # mark_values_diff = torch.diff(mark_values) + 1
-        # phase_inc = mark_values_diff / periods
-
-        # alter_value = []
-        # for i in range(wrapped_phase.shape[0]):
-        #     batch_mask = batch_idx == i
-        #     mask_i = batch_mask[:-1] & valid_mask
-        #     if not torch.any(mask_i):
-        #         alter_value.append(wrapped_phase[i])
-        #         continue
-        #     expand_phase_inc = phase_inc[mask_i].repeat_interleave(periods[mask_i])
-        #     smooth_phase = (
-        #         torch.cumsum(expand_phase_inc, 0)
-        #         + mark_values[torch.nonzero(mask_i)[0]]
-        #     ) % 1
-
-        #     alter_value_i = wrapped_phase.new_zeros(wrapped_phase.shape[1])
-        #     time_idx_i = time_idx[batch_mask]
-        #     alter_value_i[time_idx_i[0] + 1 : time_idx_i[-1]] = smooth_phase[:-1]
-        #     alter_value.append(alter_value_i)
-
-        # smooth_wrapped_phase = AudioTensor(torch.stack(alter_value, 0))
-
-        logits = self.f0_encoder(x.unsqueeze(1))
-        f0 = self.logits2f0(logits[..., 0])
-        phase_offset = torch.sigmoid(
-            AudioTensor(
-                self.offset_downsampler(logits[..., 1:].as_tensor().mT).mT,
-                hop_length=logits.hop_length * 8,
-            )
-        )
-        freq = (
-            (f0 / self.sample_rate)
-            .reduce_hop_length()
-            .unfold(phase_offset.hop_length, phase_offset.hop_length)
-        )
-        # freq._data = freq._data.detach()
-        padded_freq = F.pad(freq, (1, 0), "constant", 0)
-        wrapped_phase = (torch.cumsum(padded_freq, 2) + phase_offset) % 1
-        wrapped_phase, pred_next_phase = wrapped_phase[..., :-1], wrapped_phase[..., -1]
-        wrapped_phase = AudioTensor(
-            wrapped_phase.as_tensor().reshape(wrapped_phase.shape[0], -1)
-        )
-
-        phase_match_loss = (
-            torch.abs((pred_next_phase - phase_offset[:, 1:] + 0.5) % 1 - 0.5)
-            .as_tensor()
-            .mean()
-        )
-
-        feats = self.feature_trsfm(x)
-        logits = self.feature_encoder(feats).split(self._split_size, dim=2)
-        logits = [feats.new_tensor(torch.squeeze(l, 2)) for l in logits]
-
-        groupped_logits = []
-        for splits in self.split_tuples:
-            groupped_logits.append(logits[: len(splits)])
-            logits = logits[len(splits) :]
-
-        harm_osc_params, noise_params, noise_filter_params, end_filter_params = map(
-            lambda x: x[0](*x[1]), zip(self.trsfms, groupped_logits)
-        )
-
-        harm_osc = self.harm_oscillator(wrapped_phase, *harm_osc_params)
-        src = harm_osc + self.noise_filter(
-            self.noise_generator(harm_osc, *noise_params), *noise_filter_params
-        )
-        recon = self.end_filter(src, *end_filter_params)
-
-        return phase_offset, f0, wrapped_phase, phase_match_loss, recon
+        y = self.decoder(**params)
+        return y, None if x is None else enc_params
 
     def f0_loss(self, f0_hat, f0):
         return F.l1_loss(torch.log(f0_hat + 1e-3), torch.log(f0 + 1e-3))
 
     def training_step(self, batch, batch_idx):
         x, f0_in_hz = batch
-        f0_in_hz[f0_in_hz == 0] = 1
-        mask = f0_in_hz > 50
 
-        *_, f0_hat, wrapped_phase, phase_match_loss, x_hat = self(x)
-        # f0_hat = (torch.diff(wrapped_phase, dim=1) + 1) % 1 * self.sample_rate
-        x_hat = x_hat.as_tensor()
-        low_res_f0 = f0_in_hz[:, :: f0_hat.hop_length]
-        low_res_mask = mask[:, :: f0_hat.hop_length]
-        f0_hat = f0_hat.as_tensor()[:, : low_res_f0.shape[-1]]
-        low_res_f0 = low_res_f0[:, : f0_hat.shape[-1]]
-        low_res_mask = low_res_mask[:, : f0_hat.shape[-1]]
+        # convert to AudioTensor
+        x = AudioTensor(x)
+        f0_in_hz = AudioTensor(f0_in_hz)
 
-        x = x[:, : x_hat.shape[-1]]
-        mask = mask[:, : x_hat.shape[1]]
-        x_hat = x_hat[:, : x.shape[1]]
-        loss = self.criterion(x_hat, x) + phase_match_loss
-        l1_loss = torch.sum(mask.float() * (x_hat - x).abs()) / mask.count_nonzero()
+        params: Dict = self.encoder(x, f0=f0_in_hz if self.train_with_true_f0 else None)
+        f0_hat = params.pop("f0", None)
 
-        # f0_in_hz = f0_in_hz[:, : f0_hat.shape[1]]
-        f0_loss = self.f0_loss(f0_hat[low_res_mask], low_res_f0[low_res_mask])
+        if self.train_with_true_f0:
+            phase = torch.where(f0_in_hz == 0, 150, f0_in_hz) / self.sample_rate
+        elif self.detach_f0:
+            phase = f0_hat.new_tensor(f0_hat.as_tensor().detach()) / self.sample_rate
+        else:
+            phase = f0_hat / self.sample_rate
+        params["phase"] = phase
 
-        self.log(
-            "train_phase_match_loss", phase_match_loss, prog_bar=True, sync_dist=True
-        )
-        self.log("train_l1_loss", l1_loss, prog_bar=True, sync_dist=True)
-        self.log("train_f0_loss", f0_loss, prog_bar=True, sync_dist=True)
-        if self.l1_loss_weight > 0:
-            loss = loss + l1_loss * self.l1_loss_weight
-        if self.f0_loss_weight > 0:
+        voicing_logits = params.pop("voicing_logits", None)
+        if voicing_logits is not None:
+            voicing = torch.sigmoid(voicing_logits)
+            if self.detach_voicing:
+                voicing = voicing.detach()
+            params["voicing"] = voicing
+
+        x_hat = self.decoder(**params)
+        loss = self.criterion(x_hat[:, : x.shape[1]], x[:, : x_hat.shape[1]])
+
+        if f0_hat is not None:
+            f0_target = f0_in_hz[:, :: f0_hat.hop_length].as_tensor()[
+                :, : f0_hat.shape[1]
+            ]
+            f0_pred = f0_hat.as_tensor()[:, : f0_target.shape[1]]
+            mask = f0_target > 50
+            f0_loss = self.f0_loss(f0_pred[mask], f0_target[mask])
             loss = loss + f0_loss * self.f0_loss_weight
+            self.log("train_f0_loss", f0_loss, prog_bar=True, sync_dist=True)
+
+        if voicing_logits is not None:
+            voicing_target = (f0_in_hz > 50).float()
+            voicing_target = voicing_target[
+                :, :: voicing_logits.hop_length
+            ].as_tensor()[:, : voicing_logits.shape[1]]
+            voicing_logits = voicing_logits.as_tensor()[:, : voicing_target.shape[1]]
+            voicing_loss = F.binary_cross_entropy_with_logits(
+                voicing_logits, voicing_target, reduction="mean"
+            )
+            loss = loss + voicing_loss * self.voicing_loss_weight
+            self.log("train_voicing_loss", voicing_loss, prog_bar=True, sync_dist=True)
 
         self.log("train_loss", loss, prog_bar=True, sync_dist=True)
         return loss
@@ -250,51 +148,61 @@ class VoiceAutoEncoder(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, f0_in_hz = batch
-        f0_in_hz[f0_in_hz == 0] = 1
-        mask = f0_in_hz > 50
 
-        *_, f0_hat, wrapped_phase, phase_match_loss, x_hat = self(x)
-        # f0_hat = (torch.diff(wrapped_phase, dim=1) + 1) % 1 * self.sample_rate
-        x_hat = x_hat.as_tensor()
-        low_res_f0 = f0_in_hz[:, :: f0_hat.hop_length]
-        low_res_mask = mask[:, :: f0_hat.hop_length]
-        f0_hat = f0_hat.as_tensor()[:, : low_res_f0.shape[-1]]
-        low_res_f0 = low_res_f0[:, : f0_hat.shape[-1]]
-        low_res_mask = low_res_mask[:, : f0_hat.shape[-1]]
+        # convert to AudioTensor
+        x = AudioTensor(x)
+        f0_in_hz = AudioTensor(f0_in_hz)
 
-        x = x[:, : x_hat.shape[-1]]
-        mask = mask[:, : x_hat.shape[1]]
-        x_hat = x_hat[:, : x.shape[1]]
-        loss = self.criterion(x_hat, x) + phase_match_loss
-        l1_loss = torch.sum(mask.float() * (x_hat - x).abs()) / mask.count_nonzero()
+        if self.train_with_true_f0:
+            phase = torch.where(f0_in_hz == 0, 150, f0_in_hz) / self.sample_rate
+            x_hat, enc_params = self(x, f0_in_hz, {"phase": phase})
+        else:
+            x_hat, enc_params = self(x)
+        loss = self.criterion(x_hat[:, : x.shape[1]], x[:, : x_hat.shape[1]])
 
-        # f0_in_hz = f0_in_hz[:, : f0_hat.shape[1]]
-        f0_loss = self.f0_loss(f0_hat[low_res_mask], low_res_f0[low_res_mask])
-
-        if self.l1_loss_weight > 0:
-            loss = loss + l1_loss * self.l1_loss_weight
-        if self.f0_loss_weight > 0:
+        val_outputs = []
+        if "f0" in enc_params:
+            f0_hat = enc_params["f0"]
+            f0_target = f0_in_hz[:, :: f0_hat.hop_length].as_tensor()[
+                :, : f0_hat.shape[1]
+            ]
+            f0_pred = f0_hat.as_tensor()[:, : f0_target.shape[1]]
+            mask = f0_target > 50
+            f0_loss = self.f0_loss(f0_pred[mask], f0_target[mask])
             loss = loss + f0_loss * self.f0_loss_weight
+            val_outputs.append(f0_loss)
 
-        self.tmp_val_outputs.append((loss, l1_loss, f0_loss, phase_match_loss))
+        if "voicing_logits" in enc_params:
+            voicing_logits = enc_params["voicing_logits"]
+            voicing_target = (f0_in_hz > 50).float()
+            voicing_target = voicing_target[
+                :, :: voicing_logits.hop_length
+            ].as_tensor()[:, : voicing_logits.shape[1]]
+            voicing_logits = voicing_logits.as_tensor()[:, : voicing_target.shape[1]]
+            voicing_loss = F.binary_cross_entropy_with_logits(
+                voicing_logits, voicing_target, reduction="mean"
+            )
+            loss = loss + voicing_loss * self.voicing_loss_weight
+            val_outputs.append(voicing_loss)
+
+        val_outputs.insert(0, loss)
+
+        self.tmp_val_outputs.append(val_outputs)
 
         return loss
 
     def on_validation_epoch_end(self) -> None:
         outputs = self.tmp_val_outputs
         avg_loss = sum(x[0] for x in outputs) / len(outputs)
-        avg_l1_loss = sum(x[1] for x in outputs) / len(outputs)
-        avg_f0_loss = sum(x[2] for x in outputs) / len(outputs)
-        avg_phase_match_loss = sum(x[3] for x in outputs) / len(outputs)
-
         self.log("val_loss", avg_loss, prog_bar=True, sync_dist=True)
-        self.log("val_l1_loss", avg_l1_loss, prog_bar=False, sync_dist=True)
-        self.log("val_f0_loss", avg_f0_loss, prog_bar=False, sync_dist=True)
-        self.log(
-            "val_phase_match_loss",
-            avg_phase_match_loss,
-            prog_bar=False,
-            sync_dist=True,
-        )
+        if len(outputs[0]) > 1:
+            avg_f0_loss = sum(x[1] for x in outputs) / len(outputs)
+            self.log("val_f0_loss", avg_f0_loss, prog_bar=True, sync_dist=True)
+
+        if len(outputs[0]) > 2:
+            avg_voicing_loss = sum(x[2] for x in outputs) / len(outputs)
+            self.log(
+                "val_voicing_loss", avg_voicing_loss, prog_bar=True, sync_dist=True
+            )
 
         delattr(self, "tmp_val_outputs")

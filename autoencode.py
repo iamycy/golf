c1b5b5468048bd63410a4151c2b7a8587eb66ee4
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-09-30 13:19:00 +0100

    feat: autoencoder cli

---
+++ autoencode.py
@@ -0,0 +1,29 @@
+import lightning.pytorch as pl
+from lightning.pytorch.strategies import DDPStrategy
+from lightning.pytorch.loggers.wandb import WandbLogger
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.cli import SaveConfigCallback
+from lightning.pytorch import Trainer, LightningModule
+from lightning.fabric.utilities.cloud_io import get_filesystem
+import pathlib
+import os
+
+from ltng.ae import VoiceAutoEncoder, VoiceAutoEncoderCLI
+from main import MyConfigCallback
+
+if __name__ == "__main__":
+    cli = VoiceAutoEncoderCLI(
+        VoiceAutoEncoder,
+        # subclass_mode_model=True,
+        trainer_defaults={
+            "accelerator": "gpu",
+            "strategy": DDPStrategy(find_unused_parameters=False),
+            "log_every_n_steps": 1,
+            # "logger": WandbLogger(
+            #     project="golf",
+            #     log_model="all",
+            # ),
+        },
+        save_config_callback=MyConfigCallback,
+        save_config_kwargs={"overwrite": True},
+    )

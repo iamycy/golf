3264a1c3ed93c605b2342fe6cb6097f98d8d08a4
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-10-05 22:56:32 +0100

    omegaconf as parser

--- autoencode.py
+++ autoencode.py
@@ -26,4 +26,5 @@ if __name__ == "__main__":
         },
         save_config_callback=MyConfigCallback,
         save_config_kwargs={"overwrite": True},
+        parser_kwargs={"parser_mode": "omegaconf"},
     )

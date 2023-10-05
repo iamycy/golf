0a505786c4c791896fc788ce56dea5ef82475bb9
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2023-10-05 22:34:11 +0100

    refactor: functional logits transform

--- models/enc.py
+++ models/enc.py
@@ -2,9 +2,17 @@ import torch
 from torch import nn, Tensor
 import math
 from importlib import import_module
-from typing import Optional, Union, List, Tuple, Callable, Any
+from typing import Optional, Union, List, Tuple, Callable, Any, Dict
+from torchaudio.transforms import Spectrogram
+from itertools import accumulate, tee
 
-from .utils import get_logits2biquads, biquads2lpc, AudioTensor, rc2lpc
+from .utils import get_logits2biquads, biquads2lpc, AudioTensor, rc2lpc, get_window_fn
+
+
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 class BackboneModelInterface(nn.Module):
@@ -27,18 +35,26 @@ class VocoderParameterEncoderInterface(nn.Module):
         self,
         backbone_type: str,
         learn_voicing: bool = False,
+        learn_f0: bool = True,
         f0_min: float = 80,
         f0_max: float = 1000,
         split_sizes: Tuple[Tuple[int, ...], ...] = (),
         trsfms: Tuple[Callable[..., Tuple[torch.Tensor, ...]], ...] = (),
+        args_keys: Tuple[str, ...] = (),
         **kwargs,
     ):
         super().__init__()
         self.split_sizes = split_sizes
         self.trsfms = trsfms
+        self.args_keys = args_keys
         concated_split_sizes = sum(split_sizes, ())
-        self._split_size = ((1, 1) if learn_voicing else (1,)) + concated_split_sizes
+        if learn_voicing:
+            concated_split_sizes = (1,) + concated_split_sizes
+        if learn_f0:
+            concated_split_sizes = (1,) + concated_split_sizes
+        self._split_size = concated_split_sizes
         self.learn_voicing = learn_voicing
+        self.learn_f0 = learn_f0
         self.log_f0_min = math.log(f0_min)
         self.log_f0_max = math.log(f0_max)
 
@@ -57,22 +73,125 @@ class VocoderParameterEncoderInterface(nn.Module):
         )
 
     def forward(
-        self, h: AudioTensor
-    ) -> Tuple[AudioTensor, ...,]:
-        f0_logits, *_ = [
+        self, x: AudioTensor, *args: Any, **kwargs: Any
+    ) -> Dict[str, Union[AudioTensor, Tuple[AudioTensor]]]:
+        h = self.backbone(x, *args, **kwargs)
+        logits = [
             h.new_tensor(torch.squeeze(t, 2))
-            for t in self.backbone(h.as_tensor()).split(self._split_size, dim=2)
+            for t in torch.split(h, self._split_size, dim=2)
         ]
-        f0 = self.logits2f0(f0_logits)
+        params = {}
+        if self.learn_f0:
+            f0_logits, *logits = logits
+            params["f0"] = self.logits2f0(f0_logits)
+
         if self.learn_voicing:
-            voicing, *_ = _
-        else:
-            voicing = None
+            voicing_logits, *logits = logits
+            params["voicing_logits"] = voicing_logits
 
-        groupped_logits = []
-        for splits in self.split_sizes:
-            groupped_logits.append(_[: len(splits)])
-            _ = _[len(splits) :]
+        params.update(
+            dict(
+                zip(
+                    self.args_keys,
+                    map(
+                        lambda f, args: f(*args),
+                        self.trsfms,
+                        (
+                            logits[i:j]
+                            for i, j in pairwise(
+                                accumulate(
+                                    map(lambda x: len(x), self.split_sizes), initial=0
+                                )
+                            )
+                        ),
+                    ),
+                )
+            )
+        )
+        return params
 
-        transformed = map(lambda x: x[0](*x[1]), zip(self.trsfms, groupped_logits))
-        return (f0,) + tuple(transformed) + (voicing,)
+
+class F0EnergyEncoder(BackboneModelInterface):
+    def __init__(
+        self,
+        out_channels: int,
+        sr: int = 24000,
+        n_fft: int = 2048,
+        win_length: int = 960,
+        window: str = "hanning",
+        hop_length: int = 240,
+        num_bands: int = 150,
+        lstm_hidden_size: int = 128,
+        **lstm_kwargs,
+    ):
+        super().__init__(lstm_hidden_size * 2, out_channels)
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.sr = sr
+        self.num_bands = num_bands
+        self.freq_interval = sr / n_fft
+        self.spectrogram = Spectrogram(
+            n_fft=n_fft,
+            win_length=win_length,
+            window_fn=get_window_fn(window),
+            hop_length=hop_length,
+            center=True,
+        )
+
+        self.lstm = nn.LSTM(
+            num_bands * 2 + 1,
+            lstm_hidden_size,
+            batch_first=True,
+            bidirectional=True,
+            **lstm_kwargs,
+        )
+        self.norm = nn.LayerNorm(lstm_hidden_size * 2)
+
+        self.register_buffer("log_energy_min", torch.tensor(torch.inf))
+        self.register_buffer("log_energy_max", torch.tensor(-torch.inf))
+
+    def forward(self, x: AudioTensor, f0: AudioTensor) -> AudioTensor:
+        assert x.hop_length == 1
+        spec = self.spectrogram(x.as_tensor()).mT
+        spec[..., -1] = 0
+        f0 = f0.set_hop_length(self.hop_length).truncate(spec.size(1)).as_tensor()
+        spec = spec[:, : f0.size(1)]
+        f0_nonzero = torch.where(f0 > 0, f0, self.sr / self.num_bands * 0.5)
+        harms = f0_nonzero.unsqueeze(-1) * torch.arange(
+            1, self.num_bands + 0.5, 0.5, device=f0.device
+        )
+        harms = torch.cat([harms[..., :1] * 0.5, harms], dim=-1)
+        harms_index = (
+            torch.round(harms / self.freq_interval).long().clip(0, spec.size(-1) - 1)
+        )
+        flatten_indexes = (
+            (
+                torch.arange(
+                    spec.shape[0], device=harms_index.device, dtype=harms_index.dtype
+                ).unsqueeze(-1)
+                * spec.shape[1]
+                + torch.arange(
+                    spec.shape[1], device=harms_index.device, dtype=harms_index.dtype
+                )
+            ).unsqueeze(-1)
+            * spec.shape[2]
+            + harms_index
+        ).flatten()
+        harms_energy = spec.flatten()[flatten_indexes].reshape(*harms_index.shape)
+        log_energy = torch.log(harms_energy + 1e-8)
+        if self.training:
+            self.log_energy_min.fill_(
+                min(self.log_energy_min, torch.min(log_energy).item())
+            )
+            self.log_energy_max.fill_(
+                max(self.log_energy_max, torch.max(log_energy).item())
+            )
+        feature = (log_energy - self.log_energy_min) / (
+            self.log_energy_max - self.log_energy_min
+        )
+        log_f0 = torch.log(f0_nonzero)
+        feature = torch.cat([feature, log_f0.unsqueeze(-1)], dim=-1)
+
+        h = self.lstm(feature)[0]
+        h = self.norm(h)
+        return AudioTensor(super().forward(h), hop_length=self.hop_length)

f7143dc8c3ad3f3396fd8e80269c0f13c2cb3650
Chin-Yun Yu <cy006@bluck.eecs.qmul.ac.uk>
2024-01-01 09:02:31 +0000

    draft: evaluation code

--- ltng/ae.py
+++ ltng/ae.py
@@ -205,3 +205,100 @@ class VoiceAutoEncoder(pl.LightningModule):
 
     def load_state_dict(self, state_dict: Mapping[str, Any], **kwargs):
         return super().load_state_dict(state_dict, False)
+
+    def on_test_start(self) -> None:
+        frechet = FrechetAudioDistance(
+            use_pca=False, use_activation=False, verbose=True
+        )
+        frechet.model = frechet.model.to(self.device)
+        self.frechet = frechet
+
+        self.tmp_test_outputs = []
+
+        return super().on_test_start()
+
+    def test_step(self, batch, batch_idx):
+        x, f0_in_hz = batch
+
+        # convert to AudioTensor
+        x = AudioTensor(x)
+        f0_in_hz = AudioTensor(f0_in_hz)  # .set_hop_length(self.sample_rate // 200)
+
+        if self.train_with_true_f0:
+            phase = torch.where(f0_in_hz == 0, 150, f0_in_hz) / self.sample_rate
+            x_hat, enc_params = self(x, f0_in_hz, {"phase": phase})
+        else:
+            x_hat, enc_params = self(x)
+        loss = self.criterion(x_hat[:, : x.shape[1]], x[:, : x_hat.shape[1]]).item()
+
+        x_hat = x_hat.as_tensor().cpu().numpy().astype(np.float64)
+        x = x.as_tensor().cpu().numpy().astype(np.float64)
+        N = x_hat.shape[0]
+
+        f0_hat = np.stack(
+            [get_f0(x_hat[i], self.sample_rate, f0_floor=60)[0] for i in range(N)],
+            axis=0,
+        )
+        x_true_embs = (
+            torch.cat(
+                [self.frechet.model.forward(x[i], self.sample_rate) for i in range(N)],
+                dim=0,
+            )
+            .cpu()
+            .numpy()
+        )
+        x_hat_embs = (
+            torch.cat(
+                [
+                    self.frechet.model.forward(x_hat[i], self.sample_rate)
+                    for i in range(N)
+                ],
+                dim=0,
+            )
+            .cpu()
+            .numpy()
+        )
+
+        f0_in_hz = (
+            f0_in_hz.set_hop_length(self.sample_rate // 200)
+            .as_tensor()
+            .cpu()
+            .numpy()[:, : f0_hat.shape[1]]
+        )
+        f0_hat = f0_hat[:, : f0_in_hz.shape[1]]
+        f0_in_hz = np.maximum(f0_in_hz, 60)
+        f0_hat = np.maximum(f0_hat, 60)
+        f0_loss = np.mean(np.abs(freq2cent(f0_hat) - freq2cent(f0_in_hz)))
+
+        self.tmp_test_outputs.append((loss, f0_loss, x_true_embs, x_hat_embs, N))
+
+        return loss, f0_loss, x_true_embs, x_hat_embs, N
+
+    def on_test_epoch_end(self) -> None:
+        outputs = self.tmp_test_outputs
+        weights = [x[4] for x in outputs]
+        avg_mss_loss = np.average([x[0] for x in outputs], weights=weights)
+        avg_f0_loss = np.average([x[1] for x in outputs], weights=weights)
+
+        x_true_embs = np.concatenate([x[2] for x in outputs], axis=0)
+        x_hat_embs = np.concatenate([x[3] for x in outputs], axis=0)
+
+        mu_background, sigma_background = self.frechet.calculate_embd_statistics(
+            x_hat_embs
+        )
+        mu_eval, sigma_eval = self.frechet.calculate_embd_statistics(x_true_embs)
+        fad_score = self.frechet.calculate_frechet_distance(
+            mu_background, sigma_background, mu_eval, sigma_eval
+        )
+
+        self.log_dict(
+            {
+                "avg_mss_loss": avg_mss_loss,
+                "avg_f0_loss": avg_f0_loss,
+                "fad_score": fad_score,
+            },
+            prog_bar=True,
+            sync_dist=True,
+        )
+        delattr(self, "tmp_test_outputs")
+        return
